3e3f1f902331703d7e5157b6051e092ca0a30989
Oleksii 'LXj' Aleksieiyev <gotletter@gmail.com>
2020-06-05 23:38:56 +0300

    Naive implementation of deployment via docker-compose

--- cycl/cycl.py
+++ cycl/cycl.py
@@ -1,10 +1,21 @@
 from pathlib import Path
-from typing import Dict, Optional
+from typing import Dict, List, Optional
 
 import plumbum.cmd
 import typer
 import yaml
+from plumbum import FG, SshMachine
 from pydantic import BaseModel
+
+
+class AppDeploymentSettings(BaseModel):
+    server = "server"
+    directory = "example"
+    restart_services: List[str] = []
+    compose_file = "docker-compose-deploy.yml"
+    app_host = "example.com"
+    app_port = 12345
+    branch = "master"
 
 
 class Server(BaseModel):
@@ -14,13 +25,14 @@ class Server(BaseModel):
 class Globals(BaseModel):
     config_path = typer.get_app_dir("cycl")
     servers: Dict[str, Server] = {}
+    app = AppDeploymentSettings()
 
 
 conf = Globals()
 app = typer.Typer()
 
 
-def load_servers() -> None:
+def load_settings() -> None:
     config_dir = Path(conf.config_path)
     if not config_dir.exists():
         typer.echo("Warning: configuration directory doesn't exist!")
@@ -35,6 +47,18 @@ def load_servers() -> None:
         conf.servers = {
             name: Server(**server) for name, server in config["servers"].items()
         }
+    app_settings_path = Path.cwd() / "cycl.yaml"
+    if not app_settings_path.exists():
+        typer.echo("Warning: app deployment settings not found")
+        return
+    with app_settings_path.open("r") as app_settings_file:
+        app_settings = yaml.safe_load(app_settings_file)
+        conf.app = AppDeploymentSettings(**app_settings["deploy"])
+
+
+def get_ssh() -> SshMachine:
+    server = conf.servers[conf.app.server]
+    return SshMachine(server.host)
 
 
 @app.command()
@@ -48,11 +72,59 @@ def list_servers():
         typer.echo(f"{name}: {server.host}")
 
 
+@app.command()
+def remote_logs():
+    """
+    Show logs for remote docker-compose
+    """
+    with get_ssh() as ssh:
+        with ssh.cwd(ssh.cwd / conf.app.directory):
+            docker_compose = ssh["docker-compose"]
+            docker_compose["-f", conf.app.compose_file, "logs", "-f", "--tail=100"] & FG
+
+
+@app.command()
+def deploy_update():
+    """
+    Update remote deployment, restarts only servers in deploy.servers
+    """
+    with get_ssh() as ssh:
+        with ssh.cwd(ssh.cwd / conf.app.directory):
+            git = ssh["git"]
+            docker_compose = ssh["docker-compose"]
+            git["checkout", "."] & FG
+            git["pull"] & FG
+            git["checkout", conf.app.branch] & FG
+            git["pull"] & FG
+            docker_compose["-f", conf.app.compose_file, "build"] & FG
+            docker_compose[
+                "-f", conf.app.compose_file, "stop", conf.app.restart_services
+            ] & FG
+            docker_compose["-f", conf.app.compose_file, "up", "-d"] & FG
+
+
+@app.command()
+def full_update():
+    """
+    Update remote deployment, rebuilds with --no-cache, restarts ALL servers
+    """
+    with get_ssh() as ssh:
+        with ssh.cwd(ssh.cwd / conf.app.directory):
+            git = ssh["git"]
+            docker_compose = ssh["docker-compose"]
+            git["checkout", "."] & FG
+            git["checkout", conf.app.branch] & FG
+            git["pull"] & FG
+            docker_compose["-f", conf.app.compose_file, "build", "--no-cache"] & FG
+            docker_compose["-f", conf.app.compose_file, "down"] & FG
+            docker_compose["-f", conf.app.compose_file, "up", "-d"] & FG
+
+
 @app.callback()
 def main(config_path: Optional[str] = None):
     if config_path:
         conf.config_path = config_path
-    load_servers()
+    load_settings()
 
 
 if __name__ == "__main__":
